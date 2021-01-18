338331c092d33653983eb7d959f5fc5cf3f18023
Marek Urbaniec <marek.urbaniec1@gmail.com>
2021-01-18 18:42:18 +0100

    add getting first two cards
    Add draw first two cards from deck, add function to apiService  that allows to fetch cards from external API

--- src/Deck.js
+++ src/Deck.js
@@ -3,11 +3,33 @@ import apiService from "./services/apiService";
 
 function Deck() {
   const [deckId, setDeckId] = useState("");
+  const [dealerHand, setDealerHand] = useState("");
+  const [userHand, setUserHand] = useState("");
+
   let id;
   const startGame = async () => {
     id = await apiService.getId();
     console.log(typeof id);
-    setDeckId(id);
+    await setDeckId(id);
+    getFirstCards(id);
+  };
+
+  const getFirstCards = async (id) => {
+    if (id !== undefined) {
+      apiService.drawCard(id);
+      let { data: dealerCards } = await apiService.drawCard(id, 2);
+      if (!dealerCards) {
+        dealerCards = await apiService.drawCard(id, 2);
+      }
+      let { data: userCards } = await apiService.drawCard(id, 2);
+      if (!userCards) {
+        userCards = await apiService.drawCard(id, 2);
+      }
+      console.log(userCards.cards);
+      console.log(dealerCards.cards);
+      setUserHand(userCards.cards);
+      setDealerHand(dealerCards.cards);
+    }
   };
 
   return (
@@ -15,6 +37,22 @@ function Deck() {
       Click button to start game:
       <button onClick={startGame}>START</button>
       {deckId ? <h1>Game id: {deckId}</h1> : <></>}
+      {dealerHand ? <h1> Dealer cards</h1> : <></>}
+      {dealerHand ? (
+        dealerHand.map((card) => {
+          return <img key={card.code} src={card.image} alt={card.code} />;
+        })
+      ) : (
+        <></>
+      )}
+      {userHand ? <h1> User cards</h1> : <></>}
+      {dealerHand ? (
+        userHand.map((card) => {
+          return <img key={card.key} src={card.image} alt={card.code} />;
+        })
+      ) : (
+        <></>
+      )}
     </>
   );
 }
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,5 +1,17 @@
 import axios from "axios";
 const apiService = {
+  drawCard: (id) => {
+    try {
+      console.log("Getting cards for id:" + id);
+      let response = axios.get(
+        `https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`
+      );
+      console.log(response.cards);
+      return response;
+    } catch (err) {
+      console.log(err);
+    }
+  },
   getId: async () => {
     try {
       const resp = await axios.get(
