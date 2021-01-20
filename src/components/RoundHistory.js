de2fed83e4aceff7c6e3283dfa87d52d62b354c3
Marek Urbaniec <marek.urbaniec1@gmail.com>
2021-01-20 17:44:39 +0100

    remove warnings in console
    Removes warnings in console

--- src/components/RoundHistory.js
+++ src/components/RoundHistory.js
@@ -31,11 +31,43 @@ const RoundHistory = () => {
     betRef,
     roundHistory,
   ] = useContext(AppContext);
+
+  let state = {
+    deckId,
+    setDeckId,
+    deckIdRef,
+    dealerHand,
+    setDealerHand,
+    dealerHandRef,
+    userHand,
+    setUserHand,
+    userHandRef,
+    gameInProgress,
+    setGameInProgress,
+    gameInProgressRef,
+    userScore,
+    setUserScore,
+    userScoreRef,
+    dealerScore,
+    setDealerScore,
+    dealerScoreRef,
+    roundCounter,
+    setRoundCounter,
+    roundCounterRef,
+    moneyState,
+    setMoneyState,
+    moneyStateRef,
+    bet,
+    setBet,
+    betRef,
+    roundHistory,
+  };
+
   return (
     <div className="roundHistoryContainer">
       <h1>Round history</h1>
       <ul className="roundHistory_list">
-        {Array.from(roundHistory).map((history) => {
+        {Array.from(state.roundHistory).map((history) => {
           return (
             <li key={history.round}>
               round {history.round} -- {history.winner}
